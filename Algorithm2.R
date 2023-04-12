4a002d60bb93809710ba5c9eb2f7b066f38395c6
Lösener <u.c.losener1@uu.nl>
2023-04-12 11:40:13 +0200

    update algorithms, add Nullhyp

--- Algorithm2.R
+++ Algorithm2.R
@@ -1,4 +1,3 @@
-
 ################################################################################
 ###################### ALGORITHM 2 -- Binary Search ############################
 ################################################################################
@@ -11,40 +10,41 @@ library(mgcv)        # extracting vcov matrices
 library(bain)        # Bayesian estimation
 library(MASS)        # multinorm - already included in lme4?
 
-bayesian.SSD2 <- function(true.hyp = 1, n.steps = 20, m = 1000, eff.size = .8, t.points = c(0,1,2,3,4)) {
+bayesian.SSD2 <- function(n.steps = 20, m = 1000, eff.size = .8, t.points = c(0,1,2,3,4)) {
   
   start <- Sys.time()
 
-  true.hyp <- 1   # which hypothesis is true? H0: b=0; H1: b>0
-  n.steps <- 100  # number of different sample sizes to evaluate
-  m <- 1000       # number of datasets
-  eff.size <- .2  # effect size (Cohen's d): .2 (small), .5 (medium), .8 (large)
+  n.steps <- 20  # number of different sample sizes to evaluate
+  m <- 100       # number of datasets
+  eff.size <- .8  # effect size (Cohen's d): .2 (small), .5 (medium), .8 (large)
   
   t.points <- c(0,1,2,3,4) # time of measurements
   n <- length(t.points)    # number of measurements per subject
   
-  mbf10 <- numeric(n.steps) # storage for mean BFs 
-  mbf01 <- numeric(n.steps) # storage for mean BFs 
-  mbf.u <- numeric(n.steps) # storage for mean BFs 
-  mbf.c <- numeric(n.steps) # storage for mean BFs
-  mpmp.a <- numeric(n.steps) # storage for mean PMPs
-  mpmp.b <- numeric(n.steps) # storage for mean PMPs
-  mpmp.c <- numeric(n.steps) # storage for mean PMPs
+  prop.bf10.H0 <- numeric(n.steps) # storage for proportions of BFs exceeding a certain threshold
+  prop.bf01.H0 <- numeric(n.steps) # storage for proportions of BFs exceeding a certain threshold
+  medbf10.H0 <- numeric(n.steps) # storage for median BFs
+  medbf01.H0 <- numeric(n.steps) # storage for median BFs
+  medbf.1u.H0 <- numeric(n.steps) # storage for median BFs
+  medbf.1c.H0 <- numeric(n.steps) # storage for median BFs
+  medpmp.a1.H0 <- numeric(n.steps) # storage for median PMPs
+  medpmp.b1.H0 <- numeric(n.steps) # storage for median PMPs
+  medpmp.c1.H0 <- numeric(n.steps) # storage for median PMPs
   
-  quantbf10 <- numeric(n.steps) # storage for quantiles in vectors containing BFs
-  prop.bf10 <- numeric(n.steps) # storage for proportions of BFs exceeding a certain threshold
-  prop.bf01 <- numeric(n.steps) # storage for proportions of BFs exceeding a certain threshold
-  medbf10 <- numeric(n.steps) # storage for median BFs
-  medbf01 <- numeric(n.steps) # storage for median BFs
-  medbf.u <- numeric(n.steps) # storage for median BFs
-  medbf.c <- numeric(n.steps) # storage for median BFs
-  medpmp.a <- numeric(n.steps) # storage for median PMPs
-  medpmp.b <- numeric(n.steps) # storage for median PMPs
-  medpmp.c <- numeric(n.steps) # storage for median PMPs
+  prop.bf10.H1 <- numeric(n.steps) # storage for proportions of BFs exceeding a certain threshold
+  prop.bf01.H1 <- numeric(n.steps) # storage for proportions of BFs exceeding a certain threshold
+  medbf10.H1 <- numeric(n.steps) # storage for median BFs
+  medbf01.H1 <- numeric(n.steps) # storage for median BFs
+  medbf.1u.H1 <- numeric(n.steps) # storage for median BFs
+  medbf.1c.H1 <- numeric(n.steps) # storage for median BFs
+  medpmp.a1.H1 <- numeric(n.steps) # storage for median PMPs
+  medpmp.b1.H1 <- numeric(n.steps) # storage for median PMPs
+  medpmp.c1.H1 <- numeric(n.steps) # storage for median PMPs
   
   ss.seq <- rep(NA, n.steps)
   
-  models <- rep(list(rep(vector("list", m))),n.steps)
+  modelsH0 <- rep(list(rep(vector("list", m))),n.steps)
+  modelsH1 <- rep(list(rep(vector("list", m))),n.steps)
   
   set.seed(1234)      # for reproducibility
   #seeds <- vector("list", n.steps) 
@@ -54,15 +54,23 @@ bayesian.SSD2 <- function(true.hyp = 1, n.steps = 20, m = 1000, eff.size = .8, t
   sigma.u0.u1 <- 0  # covariance between sigmasq.u0 and sigmasq.u1. If positive, then individuals with higher(lower) initial values tend to have a higher (lower) rate of change over time.
   sigmasq.e <- 1    # error variance
   
-  bf10 <- rep(NA,m) # storage for BFs
-  bf01 <- rep(NA,m) # storage for BFs
-  bf.u <- rep(NA,m) # storage for BFs
-  bf.c <- rep(NA,m) # storage for BFs
-  pmp.a <- rep(NA,m) # storage for PMPs
-  pmp.b <- rep(NA,m) # storage for PMPs
-  pmp.c <- rep(NA,m) # storage for PMPs
+  bf10.H0 <- rep(NA,m) # storage for BFs
+  bf01.H0 <- rep(NA,m) # storage for BFs
+  bf.1u.H0 <- rep(NA,m) # storage for BFs
+  bf.1c.H0 <- rep(NA,m) # storage for BFs
+  pmp.a1.H0 <- rep(NA,m) # storage for PMPs
+  pmp.b1.H0 <- rep(NA,m) # storage for PMPs
+  pmp.c1.H0 <- rep(NA,m) # storage for PMPs
   
-  Nmin <- 10
+  bf10.H1 <- rep(NA,m) # storage for BFs
+  bf01.H1 <- rep(NA,m) # storage for BFs
+  bf.1u.H1 <- rep(NA,m) # storage for BFs
+  bf.1c.H1 <- rep(NA,m) # storage for BFs
+  pmp.a1.H1 <- rep(NA,m) # storage for PMPs
+  pmp.b1.H1 <- rep(NA,m) # storage for PMPs
+  pmp.c1.H1 <- rep(NA,m) # storage for PMPs
+  
+  Nmin <- 20
   Nmax <- 1000
   
   N <- numeric(n.steps)
@@ -74,13 +82,14 @@ bayesian.SSD2 <- function(true.hyp = 1, n.steps = 20, m = 1000, eff.size = .8, t
     # create data vectors
     y <- rep(NA, N[j])    # data storage
     t <- rep(t.points, N[j])
-    id <- rep.int(seq_len(N[j]), each=n)
+    id <- rep(seq_len(N[j]), each=n)
     treat <- as.numeric(as.character(gl(n=2, k=n, length=N[j]*n, labels=c(0,1))))
     dat0 <- data.frame(id, treat, t)
     
-    beta0 <- rep.int(0, N[j]*n) # average y at t0 
-    beta1 <- rep.int(0, N[j]*n) # average increase for x=0
-    beta2 <- eff.size * sqrt(sigmasq.u1) * rep(true.hyp==1, N[j]*n) # average difference in slopes between conditions
+    beta0 <- 0 # average y at t0 
+    beta1 <- 0 # average increase for x=0
+    beta2H0 <- 0                           # average difference in slopes between conditions under H0
+    beta2H1 <- eff.size * sqrt(sigmasq.u1) # average difference in slopes between conditions under H1
     
     for (i in 1:m) {
       #seeds[[i]] <- .Random.seed
@@ -89,50 +98,70 @@ bayesian.SSD2 <- function(true.hyp = 1, n.steps = 20, m = 1000, eff.size = .8, t
       u1 <- rep(multinorm[,2], each=n)
       e <- rnorm(N[j]*n, 0, sqrt(sigmasq.e))
       
-      y <- beta0 + u0 + beta1*t + beta2*treat*t + u1*t + e
-      dat <- data.frame(dat0, y)
+      yH0 <- beta0 + u0 + beta1*t + beta2H0*treat*t + u1*t + e # data-generating mechanism under H0
+      yH1 <- beta0 + u0 + beta1*t + beta2H1*treat*t + u1*t + e # data-generating mechanism under H1
+      datH0 <- data.frame(dat0, yH0)
+      datH1 <- data.frame(dat0, yH1)
       
       #inter <- lme(y ~ t + t:treat, random =~ t | id, data = dat, control = lmeControl(opt="optim"))
-      models[[j]][[i]] <- lmer(y ~ t + t:treat + (t | id), data = dat, control = lmerControl(calc.derivs = F))
+      modelsH0[[j]][[i]] <- lmer(yH0 ~ t + t:treat + (t | id), data = datH0, control = lmerControl(calc.derivs = F))
+      modelsH1[[j]][[i]] <- lmer(yH1 ~ t + t:treat + (t | id), data = datH1, control = lmerControl(calc.derivs = F))
       #if (isSingular(models[[j]][[i]])) {next} # uncomment if singular models are not to be included
       
-      est <- models[[j]][[i]]@beta[3]
-      names(est) <- c("t:treat")
-      sig <- list(as.matrix(vcov(models[[j]][[i]])[3,3]))
       
-      result <- bain(est, hypotheses <- "t:treat>0;t:treat=0", n = N[j], Sigma = sig, group_parameters = 1, joint_parameters = 0) # double check whether group and joint pars are correct
+      estH0 <- modelsH0[[j]][[i]]@beta[3]
+      estH1 <- modelsH1[[j]][[i]]@beta[3]
+      names(estH0) <- c("t:treat")
+      names(estH1) <- c("t:treat")
       
-      bf10[i] <- result$BFmatrix[1,2]
-      bf01[i] <- result$BFmatrix[2,1]
-      bf.u[i] <- result$fit$BF.u[1]
-      bf.c[i] <- result$fit$BF.c[1]
-      pmp.a[i] <- result$fit$PMPa[1]
-      pmp.b[i] <- result$fit$PMPb[1]
-      pmp.c[i] <- result$fit$PMPc[1]
+      sigH0 <- list(as.matrix(vcov(modelsH0[[j]][[i]])[3,3]))
+      sigH1 <- list(as.matrix(vcov(modelsH1[[j]][[i]])[3,3]))
+      
+      resultH0 <- bain(estH0, hypotheses <- "t:treat>0;t:treat=0", n = N[j], Sigma = sigH0, group_parameters = 1, joint_parameters = 0) 
+      resultH1 <- bain(estH1, hypotheses <- "t:treat>0;t:treat=0", n = N[j], Sigma = sigH1, group_parameters = 1, joint_parameters = 0)
+      
+      bf10.H0[i] <- resultH0$BFmatrix[1,2]
+      bf01.H0[i] <- resultH0$BFmatrix[2,1]
+      bf.1u.H0[i] <- resultH0$fit$BF.u[1]
+      bf.1c.H0[i] <- resultH0$fit$BF.c[1]
+      pmp.a1.H0[i] <- resultH0$fit$PMPa[1]
+      pmp.b1.H0[i] <- resultH0$fit$PMPb[1]
+      pmp.c1.H0[i] <- resultH0$fit$PMPc[1]
+      
+      bf10.H1[i] <- resultH1$BFmatrix[1,2]
+      bf01.H1[i] <- resultH1$BFmatrix[2,1]
+      bf.1u.H1[i] <- resultH1$fit$BF.u[1]
+      bf.1c.H1[i] <- resultH1$fit$BF.c[1]
+      pmp.a1.H1[i] <- resultH1$fit$PMPa[1]
+      pmp.b1.H1[i] <- resultH1$fit$PMPb[1]
+      pmp.c1.H1[i] <- resultH1$fit$PMPc[1]
     }
     
-    mbf10[j] <- mean(bf10, na.rm=T)
-    mbf01[j] <- mean(bf01, na.rm=T)
-    mbf.u[j] <- mean(bf.u, na.rm=T)
-    mbf.c[j] <- mean(bf.c, na.rm=T)
-    mpmp.a[j] <- mean(pmp.a, na.rm=T)
-    mpmp.b[j] <- mean(pmp.b, na.rm=T)
-    mpmp.c[j] <- mean(pmp.c, na.rm=T)
+    medbf10.H0[j] <- median(bf10.H0, na.rm=T)
+    prop.bf10.H0[j] <- length(bf10.H0[bf10.H0>BFthresh])/m
+    prop.bf01.H0[j] <- length(bf01.H0[bf01.H0>BFthresh])/m
+    medbf01.H0[j] <- median(bf01.H0, na.rm=T)
+    medbf.1u.H0[j] <- median(bf.1u.H0, na.rm=T)
+    medbf.1c.H0[j] <- median(bf.1c.H0, na.rm=T)
+    medpmp.a1.H0[j] <- median(pmp.a1.H0, na.rm=T)
+    medpmp.b1.H0[j] <- median(pmp.b1.H0, na.rm=T)
+    medpmp.c1.H0[j] <- median(pmp.c1.H0, na.rm=T)
     
-    medbf10[j] <- median(bf10, na.rm=T)
-    quantbf10[j] <- quantile(bf10, probs=0.2, na.rm=T)
-    prop.bf10[j] <- length(bf10[bf10>3])/m
-    prop.bf01[j] <- length(bf01[bf01>3])/m
-    medbf01[j] <- median(bf01, na.rm=T)
-    medbf.u[j] <- median(bf.u, na.rm=T)
-    medbf.c[j] <- median(bf.c, na.rm=T)
-    medpmp.a[j] <- median(pmp.a, na.rm=T)
-    medpmp.b[j] <- median(pmp.b, na.rm=T)
-    medpmp.c[j] <- median(pmp.c, na.rm=T)
+    medbf10.H1[j] <- median(bf10.H1, na.rm=T)
+    prop.bf10.H1[j] <- length(bf10.H1[bf10.H1>BFthresh])/m
+    prop.bf01.H1[j] <- length(bf01.H1[bf01.H1>BFthresh])/m
+    medbf01.H1[j] <- median(bf01.H1, na.rm=T)
+    medbf.1u.H1[j] <- median(bf.1u.H1, na.rm=T)
+    medbf.1c.H1[j] <- median(bf.1c.H1, na.rm=T)
+    medpmp.a1.H1[j] <- median(pmp.a1.H1, na.rm=T)
+    medpmp.b1.H1[j] <- median(pmp.b1.H1, na.rm=T)
+    medpmp.c1.H1[j] <- median(pmp.c1.H1, na.rm=T)
     
     print(N[j])
+    
     if(N[j]==Nmin+1) {break}
-    ifelse (prop.bf10[j]>.8, Nmax <- N[j], Nmin <- N[j])
+    
+    ifelse(prop.bf01.H0[j]>.8 & prop.bf10.H1[j]>.8, Nmax <- N[j], Nmin <- N[j])
       
   }
   
