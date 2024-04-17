bd7924c3793f2cab8653b9ec9294408ae804cbbb
Lösener <u.c.losener1@uu.nl>
2024-04-17 15:26:14 +0200

    add annotation

--- getbf.R
+++ getbf.R
@@ -1,38 +1,57 @@
+#===============================================================================
+# Function for data generation and Bayes Factor calculation
+#===============================================================================
+
+# The function uses the following arguments:
+# N = the total sample size (number of subjects)
+# t.points = position of the measurement occasions in time
+# var.u0 = intercept variance
+# var.u1 = slope variance
+# cov = covariance between intercept and slope variance
+# var.e = error variance
+# eff.size = effect size defined as beta/sqrt(var.u1), where beta is the coefficient of interaction
+# Bfthres = threshold a BF needs to exceed to be considered substantial
+# fraction = fraction of information used to specify prior, b = fraction/N
+# Neff = if "worst": effective sample size = N, if "best": effective sample size = N*n, 
+# where n = number of measurement occasions
+# log.grow = indicates whether to use logarithmic (TRUE) or linear growth (FALSE)
+
+# Note that this "getpower" requires loading the function "getbf" in the global environment
 
 
-getbf <- function(N, var.u0, var.u1, var.e, eff.size, fraction, BFthres, t.points, Neff, log.grow){
+getbf <- function(N, t.points, var.u0, var.u1, cov, var.e, eff.size, BFthres, fraction, Neff, log.grow){
   
-  n <- length(t.points)
-  ifelse(Neff=="worst",
-         b <- fraction/N,
+  n <- length(t.points)  # number of measurement occasions
+  ifelse(Neff=="worst",  # if Neff="worst", then Neff=N, otherwise Neff=N*n
+         b <- fraction/N,  # b fraction to specify prior = fraction / Neff
          b <- fraction/N*n)
-  ifelse(log.grow==F, 
-         t <- rep(t.points, N), 
-         ifelse(min(t.points)==0,
-                t <- rep(log(t.points+1), N),                                # if the first timepoint is zero, we add 1 to all timepoints because log(0) is undefined
-                t <- rep(log(t.points), N)
+  ifelse(log.grow==F,  # if logarithmic growth is used, take log of t.points
+         t <- rep(t.points, N),  # create time variable t
+         ifelse(min(t.points)==0,  # if the first timepoint is zero, we add 1 to all timepoints because log(0) is undefined
+                t <- rep(log(t.points+1), N), 
+                t <- rep(log(t.points), N)  # otherwise, just use log(t.points)
          )
   )
-  id <- rep(seq_len(N), each=n)                                              # create ID variable
-  treat <- as.numeric(as.character(gl(n=2, k=n, length=N*n, labels=c(0,1)))) # create treatment variable
-  dat0 <- data.frame(id, treat, t)                                           # combine into data frame
-  beta2.H1 <- eff.size * sqrt(var.u1)
+  id <- rep(seq_len(N), each=n)  # create ID variable
+  treat <- as.numeric(as.character(gl(n=2, k=n, length=N*n, labels=c(0,1))))  # create treatment variable
+  dat0 <- data.frame(id, treat, t)  # combine into data frame
+  beta2.H1 <- eff.size * sqrt(var.u1)  # create coefficient of interaction under H1 from effect size; beta2=0|H0
   
-  multinorm <- MASS::mvrnorm(n=2*N, mu=c(0,0), matrix(c(var.u0, 0, 0, var.u1), nrow=2, ncol=2))
-  u0.H0 <- rep(multinorm[1:(nrow(multinorm)/2),1], each=n)
-  u0.H1 <- rep(multinorm[(nrow(multinorm)/2+1):(nrow(multinorm)),1], each=n)
-  u1.H0 <- rep(multinorm[1:(nrow(multinorm)/2),2], each=n)
-  u1.H1 <- rep(multinorm[(nrow(multinorm)/2+1):(nrow(multinorm)),2], each=n)
-  e.H0 <- rnorm(N*n, 0, sqrt(var.e))
-  e.H1 <- rnorm(N*n, 0, sqrt(var.e))
-  y.H0 <- u0.H0 + 0*treat*t + u1.H0*t + e.H0
-  y.H1 <- u0.H1 + beta2.H1*treat*t + u1.H1*t + e.H1
-  dat.H0 <- data.frame(dat0, y.H0)
-  dat.H1 <- data.frame(dat0, y.H1)
+  multinorm <- MASS::mvrnorm(n=2*N, mu=c(0,0), matrix(c(var.u0, cov, cov, var.u1), nrow=2, ncol=2))  # draw random efects
+  u0.H0 <- rep(multinorm[1:(nrow(multinorm)/2),1], each=n)  # random intercepts for H0
+  u0.H1 <- rep(multinorm[(nrow(multinorm)/2+1):(nrow(multinorm)),1], each=n)  # random intercepts for H1
+  u1.H0 <- rep(multinorm[1:(nrow(multinorm)/2),2], each=n)  # random slopes for H0
+  u1.H1 <- rep(multinorm[(nrow(multinorm)/2+1):(nrow(multinorm)),2], each=n)  # random slopes for H1
+  e.H0 <- rnorm(N*n, 0, sqrt(var.e))  # error variance for H0
+  e.H1 <- rnorm(N*n, 0, sqrt(var.e))  # error variance for H1
+  y.H0 <- u0.H0 + 0*treat*t + u1.H0*t + e.H0  # create outcome variable y under H0
+  y.H1 <- u0.H1 + beta2.H1*treat*t + u1.H1*t + e.H1  # create outcome variable y under H1
+  dat.H0 <- data.frame(dat0, y.H0)  # add y under H0 to data frame
+  dat.H1 <- data.frame(dat0, y.H1)  # add y under H1 to data frame
   
-  models.H0 <- lmer(y.H0 ~ t + t:treat + (t | id), data = dat.H0, control = lmerControl(calc.derivs = F))
-  est.H0 <- models.H0@beta[3]
-  sig.H0 <- vcov(models.H0)[3,3]
+  models.H0 <- lmer(y.H0 ~ t + t:treat + (t | id), data = dat.H0, control = lmerControl(calc.derivs = F))  # fit MLM model under H0
+  est.H0 <- models.H0@beta[3]  # extract estimate for coefficient of interaction under H0
+  sig.H0 <- vcov(models.H0)[3,3]  # extract residual variance under H0  
   
   # calculate fits and complexities under H0
   comp0.H0 <- dnorm(0, mean=0, sd=sqrt(sig.H0/b))
@@ -47,10 +66,9 @@ getbf <- function(N, var.u0, var.u1, var.e, eff.size, fraction, BFthres, t.point
   BFs.H0 <- BFu.H0/BFu1.H0
   pmp.a.H0 <- BFu.H0/(BFu.H0 + BFu1.H0)
   
-  models.H1 <- lmer(y.H1 ~ t + t:treat + (t | id), data = dat.H1, control = lmerControl(calc.derivs = F))
-  est.H1 <- models.H1@beta[3]
-  names(est.H1) <- c("t:treat")
-  sig.H1 <- vcov(models.H1)[3,3]
+  models.H1 <- lmer(y.H1 ~ t + t:treat + (t | id), data = dat.H1, control = lmerControl(calc.derivs = F))  # fit MLM model under H1
+  est.H1 <- models.H1@beta[3]  # extract estimate for coefficient of interaction under H1
+  sig.H1 <- vcov(models.H1)[3,3]  # extract residual variance under H0
   
   # calculate fits and complexities under H1
   comp1.H1 <- 1-pnorm(0, mean=0, sd=sqrt(sig.H1/b))
@@ -65,7 +83,7 @@ getbf <- function(N, var.u0, var.u1, var.e, eff.size, fraction, BFthres, t.point
   BFs.H1 <- BFu.H1/BFu0.H1
   pmp.a.H1 <- BFu.H1/(BFu.H1 + BFu0.H1)
   
+  # return power for H0 and H1
   return(output = list(BF01 = BFs.H0,
                        BF10 = BFs.H1))
 }
-
--- getpower.R
+++ getpower.R
@@ -1,37 +1,40 @@
-# 
-# var.u0 <- .0333
-# var.u1 <- .1
-# var.e <- .0262
-# t.points <- c(0,1,2,3,4)
-# N <- 100
-# m <- 100
-# eff.size <- .8
-# fraction <- 1
-# BFthres <- 3
+#===============================================================================
+# Function for power determination for a given sample size
+#===============================================================================
+
+# The function uses the following arguments:
+# m = number of datasets created under each hypothesis
+# N = the total sample size (number of subjects)
+# t.points = position of the measurement occasions in time
+# var.u0 = intercept variance
+# var.u1 = slope variance
+# cov = covariance between intercept and slope variance
+# var.e = error variance
+# eff.size = effect size defined as beta/sqrt(var.u1), where beta is the coefficient of interaction
+# Bfthres = threshold a Bayes Factor needs to exceed to be considered substantial
+# fraction = fraction of information used to specify prior, b = fraction/N
+# Neff = if "worst": effective sample size = N, if "best": effective sample size = N*n, 
+# where n = number of measurement occasions
+# log.grow = indicates whether to use logarithmic (TRUE) or linear growth (FALSE)
+
+# Note that this "getpower" requires loading the function "getbf" in the global environment
 
 getpower <- function(m=1000, N=72, t.points=c(0,1,2,3,4), 
                          var.u0=0.0333, var.u1=.1, var.e=.0262, cov=0, eff.size=.8, 
                          BFthres=3, fraction=1, Neff="worst", log.grow=F){
   
-  source("getbf.R") #  call function to simulate data and calculate BFs
+  source("getbf.R")  # call function to simulate data and calculate BFs
   
-  Ns <- rep(N, m)  #  object to use lapply on with first argument for the function (N)
+  Ns <- rep(N, m)  # object to use lapply on with first argument for the function (N)
   suppressMessages({
-  #  for every m, execute function to simulate data and calculate BFs
-  bfs <- lapply(Ns, getbf, var.u0=var.u0, var.u1=var.u1, var.e=var.e, 
-         eff.size=eff.size, fraction=fraction, BFthres=BFthres, t.points=t.points, Neff=Neff, log.grow=log.grow)
+  # for every m, execute function to simulate data and calculate BFs
+  bfs <- lapply(Ns, getbf, t.points=t.points, var.u0=var.u0, var.u1=var.u1, cov=cov, var.e=var.e, 
+         eff.size=eff.size, fraction=fraction, BFthres=BFthres, Neff=Neff, log.grow=log.grow)
   })
-  bf0 <- sapply(bfs, "[[", 1) #  extract all BF01
-  bf1 <- sapply(bfs, "[[", 2) #  extract all BF10
+  bf0 <- sapply(bfs, "[[", 1) # extract all BF01
+  bf1 <- sapply(bfs, "[[", 2) # extract all BF10
   
-  #  return list with proportion of BFs>threshold, aka power
+  # return list with proportion of BFs>threshold, aka power
   return(list(power.H0 <- length(bf0[bf0>BFthres])/m,
               power.H1 <- length(bf1[bf1>BFthres])/m))
 }
-
-
-bm <- bench::mark(
-  dat.gen.hand(m=10000),
-  getpower(m=10000),
-  check=F
-)
